a3ce226c2f160080d6cbaafa8060108501f1ba57
Radek Kuhnel <radekkuhnel@hotmail.com>
2018-11-14 02:37:52 +0000

    added prev and next feature, and other not important changes

--- img_gallery.php
+++ img_gallery.php
@@ -56,34 +56,83 @@ $images = array(
 	
 	
 <body>
-
+<div id="image-container">
+<div id="prev-next">	
+<img src="img/abbey_wood_park_1.jpg" id="largeImg"  width="500px" height="400">
+<b id="prev">PREV</b><b id="next">NEXT</b>
+</div>
 <div class="images">
 <?php  foreach($images as $img ) { ?>
 	
-	<img src="<?php echo $img; ?>" width="90" height="90" alt="<?php ?>"> 
+	<img src="<?php echo $img; ?>" width="60" height="60" alt="<?php ?>"> 
 	
 <?php  }  ?>
 </div>
-<img src="" id="largeImg" hidden width="1000px"  height="800">
-
+</div>
 
 <script>
+	
+
+var prev = document.getElementById("prev");
+var next = document.getElementById("next");
+
+prev.addEventListener("click", function() {
+	moveImg('-');
+});
+
+next.addEventListener("click", function() {
+    moveImg('+');
+
+});
+
+
+
+var imgContainer = document.getElementById("image-container");
 var largImg = document.querySelector("#largImg");	
-var img = document.querySelectorAll('.images img');
+var img = document.querySelectorAll(".images img");
+var imgName = document.querySelector("#img-name");
+var imgWidth  = img[0].naturalWidth;
+var imgHeight = img[0].naturalHeight;	
+imgWidth  = imgWidth / 5;
+imgHeight = imgHeight / 5;
+var widthDiv = imgWidth + 12;
+largeImg.setAttribute("width", imgWidth);
+largeImg.setAttribute("height", imgHeight);
+imgContainer.style = "width:"+ widthDiv +'px';
+
+
 var len = img.length;
 for(let i = 0; i < len; i++) {
 img[i].addEventListener('click', function() {
 	
-	
+	var imgWidth  = img[i].naturalWidth;
+     var imgHeight = img[i].naturalHeight;	
+     imgWidth  = imgWidth / 5;
+     imgHeight = imgHeight / 5;
+
 	var src = img[i].src;
 	largeImg.removeAttribute("hidden");
 	largeImg.setAttribute("src", src);
-	
+	largeImg.setAttribute("width", imgWidth);
+	largeImg.setAttribute("height", imgHeight);
+	largeImg.setAttribute("width", imgWidth);
+     largeImg.setAttribute("height", imgHeight);	
 });	
 
 }
 // get width and height of image javascript 
 // set width and height according to width and height window screen
+
+
+
+function moveImg(symbol) {
+    //var index = img.indexOf(largImg.src);
+    
+	alert(largImg.src);
+}
+
+
+
 </script>
 
 
